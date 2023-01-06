8a2ed8bf7578907c8562c0bb952839df0c596bae
Matteo <88525373+V-0ID@users.noreply.github.com>
2023-01-06 19:58:20 +1100

    Update Miniplayer.lua

--- Miniplayer.lua
+++ Miniplayer.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Roblox-Spotify-Miniplayer/main/Patches/Patch%232.3.8.6.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Roblox-Spotify-Miniplayer/main/Patches/Patch%232.3.8.7.lua"))();
